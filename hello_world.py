69c5e917dfa6d3e5cc48a7a2013af9dc1289bfb0
zuccc23 <daniyaahmane@gmail.com>
2024-05-01 23:24:14 +0200

    hello world

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+Python 3.12.3 (v3.12.3:f6650f9ad7, Apr  9 2024, 08:18:47) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 1+2
+3
+>>> print("hello world")
+hello world
+>>> 3+4
+7
