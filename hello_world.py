b6682487d5347b042fae1d3a6fb121434fa61b96
zuccc23 <daniyaahmane@gmail.com>
2024-05-01 23:27:04 +0200

    evening world

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ Python 3.12.3 (v3.12.3:f6650f9ad7, Apr  9 2024, 08:18:47) [Clang 13.0.0 (clang-1
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 1+2
 3
->>> print("hello world")
+>>> print("evening world")
 hello world
 >>> 3+4
 7
