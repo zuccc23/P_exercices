77a0dfd87821ff37913b07ed051ddae116ffd7e2
zuccc23 <daniyaahmane@gmail.com>
2024-05-04 23:57:17 +0200

    3rd exo

---
+++ ex3.py
@@ -0,0 +1,13 @@
+#compare variables
+cat_age=3 
+
+
+def cat_age_calc(age):
+    if age<2 :
+        print ("kitten")
+    if 2<age>12:
+        print("adult")
+    if age<=12:
+        print("old")
+
+cat_age_calc
