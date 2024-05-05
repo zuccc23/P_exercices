e494b1738f95ead2a640d4b15a243187904243dc
zuccc23 <daniyaahmane@gmail.com>
2024-05-05 23:28:32 +0200

    fixed ex3

--- ex3.py
+++ ex3.py
@@ -1,13 +1,13 @@
-#compare variables
-cat_age=3 
+#compare variables 
 
+age= 12
 
-def cat_age_calc(age):
-    if age<2 :
+def cat_age_calc():
+    if age < 2 :
         print ("kitten")
-    if 2<age>12:
+    if 2 < age< 12:
         print("adult")
-    if age<=12:
+    if age >= 12:
         print("old")
 
-cat_age_calc
+cat_age_calc() 
